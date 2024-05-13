ca41ca78acfc2d141811a3a84900a211695821c7
Filipe Batista Dell <filipe.x@live.com>
2024-05-13 10:19:34 -0300

    criando pagina para editar configs do chat

---
+++ src/views/ChatOptionsView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="chat-wrapper w-screen h-screen">
+    <div
+      id="chat"
+      class="grid bottom-1 absolute"
+      :class="{ horizontal: shouldShowHorizontalChat }"
+    >
+      <StreamChat
+        v-for="({ message, username, extra }, index) in messages"
+        :key="index"
+        :message="message"
+        :user="username"
+        :extra="extra"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import StreamChat from '@/components/StreamChat.vue'
+import { useChat } from '@/composables/useChat'
+
+const { messages, shouldShowHorizontalChat } = useChat({
+  getMessagesFrom: 'chat'
+})
+</script>
+
+<style scoped>
+.chat-wrapper {
+  --mask: linear-gradient(transparent, rgb(0 0 0 / 100%) 30%);
+  -webkit-mask-image: var(--mask);
+  mask-image: var(--mask);
+}
+</style>
